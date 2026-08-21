de487261fd6df085deda7342294403178df475a3
Gabriel Zanetti <gabrielzanettigo@gmail.com>
2026-08-21 12:56:51 -0300

    feat: cria controller catch-all do gateway

---
+++ gateway-service/src/main/java/br/com/overload/gatewayservice/router/GatewayController.java
@@ -0,0 +1,22 @@
+package br.com.overload.gatewayservice.router;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class GatewayController {
+
+    @RequestMapping("/**")
+    public ResponseEntity<?> rotear(HttpServletRequest request) throws IOException {
+        String metodo = request.getMethod();
+        String path = request.getRequestURI();
+        byte[] corpo = request.getInputStream().readAllBytes();
+
+        return ResponseEntity.ok("Metodo: " + metodo + "| Path: " + path);
+    }
+}
